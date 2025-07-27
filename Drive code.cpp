37cdf8a42256ce6898e0b7af6e9293aa694f38e0
Terenceh153 <73199085+Terenceh153@users.noreply.github.com>
2025-07-26 21:33:17 -0700

    Update Drive code.cpp
    Corrected motor ports, added middle rollers and drive controls

--- Drive code.cpp
+++ Drive code.cpp
@@ -15,17 +15,17 @@ pros::Controller controller(pros::E_CONTROLLER_MASTER);
 * Port 10 - top right */
 
 // drive motors
-pros::MotorGroup leftMotors({1, -2, -3},
-                            pros::MotorGearset::blue);
-pros::MotorGroup rightMotors({-10, 9, 8}, pros::MotorGearset::blue);
+pros::MotorGroup rightMotors({1, -2, 3}, pros::MotorGearset::blue);
+pros::MotorGroup leftMotors({-16, 17, -18}, pros::MotorGearset::blue);
 
 // rollers
-pros::Motor bottomrollers(6, pros::MotorGearset::blue, pros::MotorUnits::degrees); // using degrees as encoder units
-pros::Motor toprollers(7, pros::MotorGearset::blue, pros::MotorUnits::degrees); // using degrees as encoder units
-
-// Inertial Sensor on port 10
-pros::Imu imu(10);
-
+pros::Motor bottomrollers(11, pros::MotorGearset::blue, pros::MotorUnits::degrees); // using degrees as encoder units
+pros::Motor midrollers(6, pros::MotorGearset::blue, pros::MotorUnits::degrees); // using degrees as encoder units
+pros::Motor backrollers(15, pros::MotorGearset::blue, pros::MotorUnits::degrees);
+pros::ADIDigitalOut basket ('A');
+// Inertial Sensor on port 7
+pros::Imu imu(7);
+/*
 // tracking wheels
 // horizontal tracking wheel encoder. Rotation sensor, port 20, not reversed
 pros::Rotation horizontalEnc(20);
@@ -35,14 +35,14 @@ pros::Rotation verticalEnc(-11);
 lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_275, -5.75);
 // vertical tracking wheel. 2.75" diameter, 2.5" offset, left of the robot (negative)
 lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_275, -2.5);
-
+*/
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
                               &rightMotors, // right motor group
-                              14, // 14 inch track width
-                              lemlib::Omniwheel::NEW_325, // using new 3.25" omnis
-                              480, // drivetrain rpm is 360
-                              8 // horizontal drift is 2. If we had traction wheels, it would have been 8
+                              14.5, // 14 inch track width
+                              lemlib::Omniwheel::NEW_4, // using new 3.25" omnis
+                              343, // drivetrain rpm is 360
+                              2 // horizontal drift is 2. If we had traction wheels, it would have been 8
 );
 
 // lateral motion controller
@@ -70,9 +70,9 @@ lemlib::ControllerSettings angularController(2, // proportional gain (kP)
 );
 
 // sensors for odometry
-lemlib::OdomSensors sensors(&vertical, // vertical tracking wheel
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel
                             nullptr, // vertical tracking wheel 2, set to nullptr as we don't have a second one
-                            &horizontal, // horizontal tracking wheel
+                            nullptr, // horizontal tracking wheel
                             nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
                             &imu // inertial sensor
 );
@@ -128,8 +128,6 @@ void movefwd(float inches, float maxspeed, int timeout = 0) {
 	}
 }
 
-
-
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -139,6 +137,8 @@ void movefwd(float inches, float maxspeed, int timeout = 0) {
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
     chassis.calibrate(); // calibrate sensors
+
+    
 
     // the default rate is 50. however, if you need to change the rate, you
     // can do the following.
@@ -180,37 +180,12 @@ ASSET(example_txt); // '.' replaced with "_" to make c++ happy
 /**
  * Runs during auto
  *
- * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
+ * 
  */
 void autonomous() {
-    // Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
-    chassis.moveToPose(20, 15, 90, 4000);
-    // Move to x: 0 and y: 0 and face heading 270, going backwards. Timeout set to 4000ms
-    chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
-    // cancel the movement after it has traveled 10 inches
-    chassis.waitUntil(10);
-    chassis.cancelMotion();
-    // Turn to face the point x:45, y:-45. Timeout set to 1000
-    // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnToPoint(45, -45, 1000, {.maxSpeed = 60});
-    // Turn to face a direction of 90º. Timeout set to 1000
-    // will always be faster than 100 (out of a maximum of 127)
-    // also force it to turn clockwise, the long way around
-    chassis.turnToHeading(90, 1000, {.direction = AngularDirection::CW_CLOCKWISE, .minSpeed = 100});
-    // Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // following the path with the back of the robot (forwards = false)
-    // see line 116 to see how to define a path
-    chassis.follow(example_txt, 15, 4000, false);
-    // wait until the chassis has traveled 10 inches. Otherwise the code directly after
-    // the movement will run immediately
-    // Unless its another movement, in which case it will wait
-    chassis.waitUntil(10);
-    pros::lcd::print(4, "Traveled 10 inches during pure pursuit!");
-    // wait until the movement is done
-    chassis.waitUntilDone();
-    pros::lcd::print(4, "pure pursuit finished!");
+    chassis.moveToPose(-23, 22, 65, 4000);
+    chassis.moveToPose(-24, 47, 282, 4000);
 }
-
 /**
  * Runs in driver control
  */
@@ -223,22 +198,30 @@ void opcontrol() {
 		
 		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
 			bottomrollers.move_velocity(500); // move bottom rollers at full speed
-			toprollers.move_velocity(-500); // move top rollers at full speed
+			midrollers.move_velocity(-500); // move top rollers at full speed
 		} else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
 			bottomrollers.move_velocity(500); // move bottom rollers at full speed
-			toprollers.move_velocity(500); // move top rollers at full speed
+			midrollers.move_velocity(500); // move top rollers at full speed
+            backrollers.move_velocity(-500);//move (actual) top roller full speed
 		} else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
 			bottomrollers.move_velocity(-500); // move bottom rollers at full speed in reverse
-			toprollers.move_velocity(-500); // move top rollers at full speed in reverse
+			midrollers.move_velocity(-500); // move top rollers at full speed in reverse@p
 		} else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            backrollers.move_velocity(500);
 			bottomrollers.move_velocity(-500); // move bottom rollers at full speed in reverse
-			toprollers.move_velocity(0); // stop top rollers
+			midrollers.move_velocity(-500); // stop top rollers
 		} else
 		
 		{
 			bottomrollers.move_velocity(0); // stop bottom rollers
-			toprollers.move_velocity(0); // stop top rollers
+			midrollers.move_velocity(0); // stop top rollers
 		}
+        if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
+            basket.set_value(true);
+        }
+        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)){
+            basket.set_value(false);
+        }
         // move the robot
         chassis.curvature(leftY, rightX);
 
